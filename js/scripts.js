6f6c40eb28b9c2b2060744e45c967a2b10808b57
geomio <geoff.millner22@gmail.com>
2021-06-03 10:30:06 -0700

    added js for loop and holder for input it will count up to a number and add it to array area

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,15 @@
-let userNumber = 12;
-
-let numbersToStrings = function(inputNumbers) {
-let numberMarker = [1, 2, 3];
-let stringArray = ['Beep!', 'Boop!', 'Wont you be my neighbor?'];
-
-let empty = '';
-for (let index = 0; index < numberMarker.length; index++) {
-  while (numberMarker[index] <= inputNumbers) {
-    empty += stringArray[index];
-    inputNumbers -= numberMarker[index];
-  }
+//area a
+let userCount = 6;
+let openArray =[]
+for (let index = 0; index <= userCount; index++) {
+    userCount[index] = index;
+    index.toString();
+    openArray.push(index);   
 }
+console.log(openArray);
 
-return empty;
-  }; 
-  console.log("if only this then broke", numbersToStrings(3, 4, 5, 1, 2));
+//Area a will count up to user input converts to a string and push it to open array
 
- 
+// these two arrays below will compare and replace perspective integers
+let compare = ['1','2','3'];
+let replace = ['Beep', 'Boop', 'Wont you be my neighbor'];
