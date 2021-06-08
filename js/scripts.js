4ffeacecf369dabfcbd907831af0298d78fb4aaa
geomio <geoff.millner22@gmail.com>
2021-06-08 11:13:40 -0700

    Separate business logic from user finish js code to take user input and print results to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,52 @@
-//area a
-let inputNumber = 55
+// //area a
+// let inputNumber = 55
 
-function masterConvert() {
-let userCount = inputNumber;
-let openArray =[]
-    for (let index = 0; index <= userCount; index++) {
+// function masterConvert() {
+// let userCount = inputNumber;
+// let openArray =[]
+//     for (let index = 0; index <= userCount; index++) {
+//         const indexString = index.toString();
+//         if(indexString.includes('3')) {
+//             openArray.push('Welcome to the neighborhood.');
+//         }else if (indexString.includes('2')) {
+//             openArray.push('Boop.');
+//         }else if (indexString.includes('1')) {
+//             openArray.push('Beep.');
+//         }else {
+//             openArray.push(index);
+//         }      
+//     }
+// console.log(openArray);
+// }
+
+function masterConvert(inputNumber) {
+        let openArray =[]
+        for (let index = 0; index <= inputNumber; index++) {
         const indexString = index.toString();
-        if(indexString.includes('3')) {
+            if(indexString.includes('3')) {
             openArray.push('Welcome to the neighborhood.');
-        }else if (indexString.includes('2')) {
+            }else if (indexString.includes('2')) {
             openArray.push('Boop.');
-        }else if (indexString.includes('1')) {
+            }else if (indexString.includes('1')) {
             openArray.push('Beep.');
-        }else {
+            }else {
             openArray.push(index);
-        }      
-    }
-console.log(openArray);
-}
+            }      
+        }
+        return openArray;
+    };
+// masterConvert();
 
-masterConvert();
-
+$(document).ready(function () {
+    
+    $("form#numbers").submit(function(event){
+    const userInputNumber = $('input#htmlNumber').val();
+    console.log(userInputNumber);
+    
+    const convertedNumber = masterConvert(userInputNumber);
+    $("#returnOutput").text(convertedNumber.join(" "));
+    // $("#returnOutput").prepend(' ' + openArray + ' ');
+    
+    event.preventDefault();
+  });
+});
